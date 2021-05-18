9e846668e790eda1a7b154de5650a1b9b52e0f06
Mohsen Zand (MZ33) <m.zand@queensu.ca>
2021-05-18 18:59:02 -0400

    make dataset generation compatible to our dataset.

--- README.md
+++ README.md
@@ -23,7 +23,12 @@ there are two fields in *.mat:
 
 - in make_dataset.py, an image-sized matrix 'k' with all zeros values is defined. All 'locations' are assigned '1' in 'k'. 
 - density of matrix 'k' is obtained using a 'gaussian_filter'.  
+- There are two types of 'gaussian_filters':
+1) line 80, which is applied on part_A. It does not work with 'people_count' less than 4. 
+2) line 109, which is applied on part_B. It is a standard implementation from scipy package. 
 - obtained density is saved in a 'h5' file in the 'density' field. 
+- You must run 'make_dataset.py' to generate the 'h5' files.
+
 
 
 
--- make_dataset.py
+++ make_dataset.py
@@ -15,15 +15,21 @@ from image import *
 from model import CSRNet
 import torch
 
-#set the root to the Shanghai dataset you download
-root = '/media/mohsen/myDrive/datasets/ShanghaiTech_Crowd_Counting_Dataset'
+CSRNet_EXP = False
+Queens_Geotab_EXP = not CSRNet_EXP
 
-#now generate the ShanghaiA's ground truth
+#set the root to the Shanghai dataset you download
+if CSRNet_EXP:
+    root = '/media/mohsen/myDrive/datasets/ShanghaiTech_Crowd_Counting_Dataset'
+
+elif Queens_Geotab_EXP: 
+    root = '/media/mohsen/myDrive/datasets/Queens_Geotab/Crowd_Counting_Sim_Dataset_01/Crowd_Counting_Sim_Dataset'
+    #root = '/media/mohsen/myDrive/datasets/Queens_Geotab/Crowd_Counting_Sim_Dataset_02/Crowd_Counting_Sim_Dataset'
+
 part_A_train = os.path.join(root,'part_A_final/train_data','images')
 part_A_test = os.path.join(root,'part_A_final/test_data','images')
 part_B_train = os.path.join(root,'part_B_final/train_data','images')
 part_B_test = os.path.join(root,'part_B_final/test_data','images')
-
 
 #this is borrowed from https://github.com/davideverona/deep-crowd-counting_crowdnet
 def gaussian_filter_density(gt):
@@ -60,12 +66,14 @@ for path in path_sets:
         img_paths.append(img_path)
 
 
-for img_path in img_paths:
+for im_n, img_path in enumerate(img_paths):
     print(img_path)
     mat = io.loadmat(img_path.replace('.jpg','.mat').replace('images','ground_truth').replace('IMG_','GT_IMG_'))
-    img= plt.imread(img_path)
+    img = plt.imread(img_path)
     k = np.zeros((img.shape[0],img.shape[1]))
     gt = mat["image_info"][0,0][0,0][0]
+    if gt.shape[0] < 4:
+        continue
     for i in range(0,len(gt)):
         if int(gt[i][1])<img.shape[0] and int(gt[i][0])<img.shape[1]:
             k[int(gt[i][1]),int(gt[i][0])]=1
@@ -73,14 +81,14 @@ for img_path in img_paths:
     with h5py.File(img_path.replace('.jpg','.h5').replace('images','ground_truth'), 'w') as hf:
             hf['density'] = k
 
-
-#now see a sample from ShanghaiA
-plt.subplot(1,2,1).imshow(Image.open(img_paths[0]))
-gt_file = h5py.File(img_paths[0].replace('.jpg','.h5').replace('images','ground_truth'),'r')
-groundtruth = np.asarray(gt_file['density'])
-plt.subplot(1,2,2).imshow(groundtruth,cmap=CM.jet)
-plt.show()
-count = np.sum(groundtruth)# don't mind this slight variation
+    '''#now see a sample
+    plt.subplot(1,2,1).imshow(Image.open(img_paths[im_n]))
+    gt_file = h5py.File(img_paths[im_n].replace('.jpg','.h5').replace('images','ground_truth'),'r')
+    groundtruth = np.asarray(gt_file['density'])
+    plt.subplot(1,2,2).imshow(groundtruth,cmap=CM.jet)
+    count = np.sum(groundtruth)# don't mind this slight variation
+    plt.title('People count: ' + str(np.round(count)))
+    plt.show()'''
 
 #now generate the ShanghaiB's ground truth
 path_sets = [part_B_train,part_B_test]
@@ -89,10 +97,10 @@ for path in path_sets:
     for img_path in glob.glob(os.path.join(path, '*.jpg')):
         img_paths.append(img_path)
 
-for img_path in img_paths:
+for im_n, img_path in enumerate(img_paths):
     print(img_path)
     mat = io.loadmat(img_path.replace('.jpg','.mat').replace('images','ground_truth').replace('IMG_','GT_IMG_'))
-    img= plt.imread(img_path)
+    img = plt.imread(img_path)
     k = np.zeros((img.shape[0],img.shape[1]))
     gt = mat["image_info"][0,0][0,0][0]
     for i in range(0,len(gt)):
@@ -101,3 +109,12 @@ for img_path in img_paths:
     k = gaussian_filter(k,15)
     with h5py.File(img_path.replace('.jpg','.h5').replace('images','ground_truth'), 'w') as hf:
             hf['density'] = k
+    
+    '''#now see a sample 
+    plt.subplot(1,2,1).imshow(Image.open(img_paths[im_n]))
+    gt_file = h5py.File(img_paths[im_n].replace('.jpg','.h5').replace('images','ground_truth'),'r')
+    groundtruth = np.asarray(gt_file['density'])
+    plt.subplot(1,2,2).imshow(groundtruth,cmap=CM.jet)
+    count = np.sum(groundtruth)# don't mind this slight variation
+    plt.title('People count: ' + str(np.round(count)))
+    plt.show()'''
